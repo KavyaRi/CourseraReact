c75e3ce2b83662af8f86266fe3749b31a8076ef2
Kavya Sri <kavyachowdary6084@gmail.com>
2022-12-30 11:08:47 +0530

    Middle commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import "bootstrap/dist/js/bootstrap.min.js";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
